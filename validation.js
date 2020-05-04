6636417ccaed6b3509f56ef11d02221f35b95f29
Pooja <PoojaKushwah1402>
2020-05-05 00:04:33 +0530

    Refactor code

--- validation.js
+++ validation.js
@@ -1,56 +1,118 @@
-function validateForm(e)
+function validateForm()
 {
     var Fname=document.forms["Form1"]["FirstName"].value;
     var Mobile=document.forms["Form1"]["Moble"].value;
     var Message=document.forms["Form1"]["message"].value;
     var email=document.forms["Form1"]["Email"].value;
+    var subject=document.forms["Form1"]["drodown"].value;
+
      Mobile=Number(Mobile);
+    
+     var name = validateFname(Fname);
+     var mob = validateMobile(Mobile);
+     var mail = validateEmail(email);
+     var msg = validateMessage(Message);
+     var sub = validateSubject(subject);
+
+    if( name && mob && mail && msg && sub)
+    {
+        sendquery();
+    }
+    
+    event.preventDefault();    
+}
+
+function validateFname(Fname)
+{
     if(!Fname)
     {
         document.forms["Form1"]["FirstName"].style.border="2px solid red";   
         document.querySelector("p1").style.visibility="visible";
+        return false;
     }
     else
     {
         document.forms["Form1"]["FirstName"].style.border="2px solid #c4d2e7";   
         document.querySelector("p1").style.visibility="hidden";  
+        return true;
     }
+}
 
+function validateSubject(subject)
+{
+    if(!subject)
+    {
+        document.forms["Form1"]["drodown"].style.border="2px solid red";   
+        document.querySelector("p5").style.visibility="visible";
+        return false;
+    }
+    else
+    {
+        document.forms["Form1"]["drodown"].style.border="2px solid #c4d2e7";   
+        document.querySelector("p5").style.visibility="hidden";  
+        return true;
+    }   
+}
+
+
+function validateMobile(Mobile)
+{
     if(!Mobile )
     {
         document.forms["Form1"]["Moble"].style.border="2px solid red";   
         document.querySelector("p2").style.visibility="visible";  
-        console.log(typeof(Mobile)); 
+        return false;
     }
     else
     {
         document.forms["Form1"]["Moble"].style.border="2px solid #c4d2e7";   
         document.querySelector("p2").style.visibility="hidden";  
+        return true;
     }
+}
 
-    if(!Message)
-    {
-        document.forms["Form1"]["message"].style.border="2px solid red";
-        document.querySelector("p4").style.visibility="visible";
-           
-    }
-    else
-    {
-        document.forms["Form1"]["message"].style.border="2px solid #c4d2e7";   
-        document.querySelector("p4").style.visibility="hidden";  
-    }
-
-    if(!email)
+function validateEmail(email)
+{  var check = email.includes("@");
+    if(!email || !check)
     {
         document.forms["Form1"]["Email"].style.border="2px solid red";  
-        document.querySelector("p3").style.visibility="visible";   
+        document.querySelector("p3").style.visibility="visible";  
+        return false; 
     }
     else
     {
         document.forms["Form1"]["Email"].style.border="2px solid #c4d2e7";   
         document.querySelector("p3").style.visibility="hidden";  
+        return true;
     }
-
-    event.preventDefault();
 }
+
+function validateMessage(Message)
+{
+    if(!Message)
+    {
+        document.forms["Form1"]["message"].style.border="2px solid red";
+        document.querySelector("p4").style.visibility="visible";
+        return false;  
+    }
+    else
+    {
+        document.forms["Form1"]["message"].style.border="2px solid #c4d2e7";   
+        document.querySelector("p4").style.visibility="hidden";  
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
